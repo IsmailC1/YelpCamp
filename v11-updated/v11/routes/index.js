dfbf2066eacf94ab1ea561c58c11ba4e090a89ad
Ismail Cucevic <cucevicismail@gmail.com>
2018-12-08 12:12:55 +0000

    add text message

--- v11-updated/v11/routes/index.js
+++ v11-updated/v11/routes/index.js
@@ -99,13 +99,16 @@ router.get('/forgot', function(req, res, next) {
                 to: user.email,
                 from: 'cucevicismail@gmail.com',
                 subject: 'Node.js Password Reset',
-                text : 'them you!'
-            };
-            smtpTransport.sendMail(mailOptions, function(err){
-                console.log('mail sent');
-                req.flash('success', 'An e-mail has been sent to' + user.email + 'with further inst.') 
-                done(err, 'done'); 
-             });
+                     text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
+          'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
+          'http://' + req.headers.host + '/reset/' + token + '\n\n' +
+          'If you did not request this, please ignore this email and your password will remain unchanged.\n'
+      };
+      smtpTransport.sendMail(mailOptions, function(err) {
+        console.log('mail sent');
+        req.flash('success', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
+        done(err, 'done');
+      });
            }
          ], function(err){
              if(err) return next(err); 
